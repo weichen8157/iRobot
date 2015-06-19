3200ddd042be23aee693516965505fe843e3b414
weichen8157 <weichen8157@gmail.com>
2015-06-19 20:45:12 +0800

    update

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <wiringPi.h>
 #include <stdlib.h>
-unsigned int count=0;
+#define DISTANCE 360
+#define NOTURN 0
+#define AFTERRIGHT 1
+#define AFTERLEFT 2
+
+unsigned int count=0,direction=0;
 unsigned int currentLeft,currentRight,currentLLeft,currentRRight;
 
 /*dimension
@@ -72,10 +77,10 @@ void backward(int in1,int in2,int in3,int in4,int in5,int in6,int in7,int in8)
 void turnRight(int in1,int in2,int in3,int in4,int in5,int in6,int in7,int in8)
 {
     digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
+    digitalWrite(in2,HIGH);
     digitalWrite(in3,HIGH);
     digitalWrite(in4,LOW);
-    digitalWrite(in5,LOW);
+    digitalWrite(in5,HIGH);
     digitalWrite(in6,LOW);
     digitalWrite(in7,HIGH);
     digitalWrite(in8,LOW);
@@ -86,10 +91,10 @@ void turnLeft(int in1,int in2,int in3,int in4,int in5,int in6,int in7,int in8)
     digitalWrite(in1,HIGH);
     digitalWrite(in2,LOW);
     digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW);
+    digitalWrite(in4,HIGH);
     digitalWrite(in5,HIGH);
     digitalWrite(in6,LOW);
-    digitalWrite(in7,LOW);
+    digitalWrite(in7,HIGH);
     digitalWrite(in8,LOW);
 }
 
@@ -104,6 +109,104 @@ void stop(int in1,int in2,int in3,int in4,int in5,int in6,int in7,int in8)
     digitalWrite(in7,LOW);
     digitalWrite(in8,LOW);
 }
+
+
+struct region{
+        char up=0;
+        char left=0; 
+        char right=0;
+        char back=0;
+        char state = 0; //0=unknown 1=free 2=obstacle;
+};
+
+typedef struct car{
+        volatile float x;// car position
+        volatile float y;
+        char mode;          // running mode,0:first run
+        char direction; // 0:North 1:East 2:South 3:West
+}CAR;
+
+void detect(){
+        unsigned int timeLeft,timeRight,timeLLeft,timeRRight;
+
+        trigger(0);
+        timeLeft = pulseIn(1,HIGH,500000);
+        currentLeft = distance(timeLeft);
+                                 
+        trigger(2);  
+        timeRight = pulseIn(3,HIGH,500000);
+        currentRight = distance(timeRight);
+
+        trigger(4);
+        timeLLeft = pulseIn(5,HIGH,500000);
+        currentLLeft = distance(timeLLeft);
+                                                                    
+        trigger(6);  
+        timeRRight = pulseIn(7,HIGH,500000);
+        currentRRight = distance(timeRRight);
+                                                                                        
+}
+
+void init(){
+        car.x=1;
+        car.y=1;
+        car.direction = 0;
+        map[0][0].state = free;
+        int i,j;
+        for(i=0;i<X;i++){
+                for(j=0;j<Y;j++){
+                         if(j==0||i==0||i==X||j=={
+                                map[i][j] = 2;                                                                                  }
+                                                                                                
+                }
+        }
+}
+
+void update_map(CAR car){
+        //¶ZÂ´«º⦨³æ
+        unsigned int Leftunit = currentLeft/unit;
+        unsigned int Rightunit = currentRight/unit;
+        unsigned int LLeftunit = currentLLeft/unit;
+        unsigned int RRightunit = currentRRight/unit;
+        unsigned int Frontunit;
+
+        //§PÂ«e¤襪or¥k¸û¨ú
+        if(rightunit>leftunit)  
+                Frontunit = Leftunit;
+        else
+                Frontunit = Rightunit;
+
+        //®ھڳæ¡A¨M©w«e«ᥪ¥kstate
+        if(Leftunit==0 && currentLLeft!=0)      //currentLLeft=0 ¶W¹L¥i´ú½d³òí¤U­ӳæµL»Ùªª«
+                map[car.x][car.y].left = 2;
+        else
+                map[car.x][car.y].left = 1;
+
+        if(Rightunit==0 && currentRRight!=0)
+                map[car.x][car.y].right = 2;
+        else
+                map[car.x][car.y].right = 1;
+
+        if(Frontunit==0 && (currentLeft!=0 || currentRight!=0))
+                map[car.x][car.y].up = 2;
+        else
+                map[car.x][car.y].up = 1;
+
+        if(car.direction==0)
+                map[car.x][car.y].back = map[car.x][car.y-1].state;
+        else if(car.direction==2)
+                map[car.x][car.y].back = map[car.x][car.y+1].state;
+}
+
+        //globalÅ¼Æ
+        unsigned int unit 1 ;                   //³æ¤j¤p
+        volatile unsigned int currentLeft,currentRight,currentLLeft,currentRRight;
+
+        CAR car;
+
+        int X 256;
+        int Y 128;      
+        struct region map[X][Y];
 
 int main (void)
 {
@@ -133,15 +236,15 @@ int main (void)
      unsigned int distLeft,distRight,distLLeft,distRRight;
 
      trigger(0);
-     timeLeft = pulseIn(1,HIGH,500000);
+     timeLeft = pulseIn(1,HIGH,50000);
      distLeft = distance(timeLeft);
      if(distLeft >0)
      {
          currentLeft=distLeft;
      }
 
-     trigger(2);  
-     timeRight = pulseIn(3,HIGH,500000);
+     trigger(2);
+     timeRight = pulseIn(3,HIGH,50000);
      distRight = distance(timeRight);
      if(distRRight >0)
      {
@@ -149,39 +252,132 @@ int main (void)
      }
 
      trigger(4);
-     timeLLeft = pulseIn(5,HIGH,500000);
+     timeLLeft = pulseIn(5,HIGH,50000);
      distLLeft = distance(timeLLeft);
      if(distLLeft >0)
      {
          currentLLeft=distLLeft;
      }
 
-     trigger(6);  
-     timeRRight = pulseIn(7,HIGH,500000);
+     trigger(6);
+     timeRRight = pulseIn(7,HIGH,50000);
      distRRight = distance(timeRRight);
      if(distRRight >0)
      {
          currentRRight=distRRight;
      }
 
-     if(currentLeft<=240 && currentRight <=240 && currentRRight <=240 && currentRRight <=240)
+
+     if (currentLeft>DISTANCE && currentRight >DISTANCE)
      {
-         backward(8,9,10,11,12,13,14,15);
+        if (currentLLeft>DISTANCE && currentRRight >DISTANCE)
+        {
+           forward(8,9,10,11,12,13,14,15);
+        }
+        else if(currentLLeft<=DISTANCE && currentRRight <=DISTANCE)
+        {
+            forward(8,9,10,11,12,13,14,15);
+        }
+        else if (currentLLeft<=DISTANCE && currentRRight >DISTANCE)
+        {
+            turnRight(8,9,10,11,12,13,14,15);
+            direction=AFTERRIGHT;
+        }
+        else
+        {
+            turnLeft(8,9,10,11,12,13,14,15);
+            direction=AFTERLEFT;
+        }
      }
-     else if (currentLeft>240 && currentRight >240)
+     else if(currentLeft<=DISTANCE && currentRight <=DISTANCE)
      {
-         forward(8,9,10,11,12,13,14,15);
+        if (currentLLeft>DISTANCE && currentRRight >DISTANCE)
+        {
+             switch (direction)
+             {
+                 case AFTERRIGHT:
+                    turnLeft(8,9,10,11,12,13,14,15);
+                    break;
+                 case AFTERLEFT:
+                    turnRight(8,9,10,11,12,13,14,15);
+                    break;
+                 default:
+                    turnRight(8,9,10,11,12,13,14,15);                    
+             }
+        }
+        else if(currentLLeft<=DISTANCE && currentRRight <=DISTANCE)
+        {
+            backward(8,9,10,11,12,13,14,15);
+        }
+        else if (currentLLeft<=DISTANCE && currentRRight >DISTANCE)
+        {
+            turnRight(8,9,10,11,12,13,14,15);
+        }
+        else
+        {
+            turnLeft(8,9,10,11,12,13,14,15);
+        }
      }
-     else if (currentLeft<=240 && currentRRight >240)
+     else if (currentLeft<=DISTANCE && currentRight >DISTANCE)
      {
-         turnRight(8,9,10,11,12,13,14,15);
+        if (currentLLeft>DISTANCE && currentRRight >DISTANCE)
+        {
+             switch (direction)
+             {
+                 case AFTERRIGHT:
+                    turnLeft(8,9,10,11,12,13,14,15);
+                    break;
+                 case AFTERLEFT:
+                    turnRight(8,9,10,11,12,13,14,15);
+                    break;
+                 default:
+                    turnRight(8,9,10,11,12,13,14,15);                    
+             }
+        }
+        else if(currentLLeft<=DISTANCE && currentRRight <=DISTANCE)
+        {
+            backward(8,9,10,11,12,13,14,15);
+        }
+        else if (currentLLeft<=DISTANCE && currentRRight >DISTANCE)
+        {
+            turnRight(8,9,10,11,12,13,14,15);
+        }
+        else
+        {
+            turnLeft(8,9,10,11,12,13,14,15);
+        }
      }
-     else if (currentRight<=240 && currentLLeft >240)
+     else
      {
-         turnLeft(8,9,10,11,12,13,14,15);
+        if (currentLLeft>DISTANCE && currentRRight >DISTANCE)
+        {
+              switch (direction)
+             {
+                 case AFTERRIGHT:
+                    turnLeft(8,9,10,11,12,13,14,15);
+                    break;
+                 case AFTERLEFT:
+                    turnRight(8,9,10,11,12,13,14,15);
+                    break;
+                 default:
+                    turnRight(8,9,10,11,12,13,14,15);                    
+             }
+        }
+        else if(currentLLeft<=DISTANCE && currentRRight <=DISTANCE)
+        {
+            backward(8,9,10,11,12,13,14,15);
+        }
+        else if (currentLLeft<=DISTANCE && currentRRight >DISTANCE)
+        {
+            turnRight(8,9,10,11,12,13,14,15);
+        }
+        else
+        {
+            turnLeft(8,9,10,11,12,13,14,15);
+        }
      }
-     printf("distLeft: %d distRight: %d count zero: %d\n",distLeft,distRight,count);
-     delay (1000);
+     printf("distLeft: %d distRight: %d distLLeft: %d distRRight: %d count zero: %d\n",distLeft,distRight,distLLeft,distRRight,count);
+     delay(100); 
   }
   return 0 ;
 }
